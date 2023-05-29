b9f0f99fd90755c6555cb2d59879e7ebcb4feabc
vomikan <nikolay.akimov@gmail.com>
2023-05-29 13:42:36 +0300

    fix(): read since specified date

--- tele_reader.py
+++ tele_reader.py
@@ -1,5 +1,10 @@
 import configparser
 import json
+import sys
+from datetime import timedelta, date
+from dateutil.parser import parse
+from pytz import timezone, utc
+import pytz
 
 from telethon.sync import TelegramClient
 from telethon import connection
@@ -29,46 +34,7 @@ client = TelegramClient(username, api_id, api_hash)
 
 client.start()
 
-
-async def dump_all_participants(channel):
-	"""Записывает json-файл с информацией о всех участниках канала/чата"""
-	offset_user = 0    # номер участника, с которого начинается считывание
-	limit_user = 100   # максимальное число записей, передаваемых за один раз
-
-	all_participants = []   # список всех участников канала
-	filter_user = ChannelParticipantsSearch('')
-
-	while True:
-		participants = await client(GetParticipantsRequest(channel,
-			filter_user, offset_user, limit_user, hash=0))
-		if not participants.users:
-			break
-		all_participants.extend(participants.users)
-		offset_user += len(participants.users)
-
-	all_users_details = []   # список словарей с интересующими параметрами участников канала
-
-	for participant in all_participants:
-		all_users_details.append({"id": participant.id,
-			"first_name": participant.first_name,
-			"last_name": participant.last_name,
-			"user": participant.username,
-			"phone": participant.phone,
-			"is_bot": participant.bot})
-
-	with open('channel_users.json', 'w', encoding='utf8') as outfile:
-		json.dump(all_users_details, outfile, ensure_ascii=False)
-
-
-async def dump_all_messages(channel):
-	"""Записывает json-файл с информацией о всех сообщениях канала/чата"""
-	offset_msg = 0    # номер записи, с которой начинается считывание
-	limit_msg = 100   # максимальное число записей, передаваемых за один раз
-
-	all_messages = []   # список всех сообщений
-	total_messages = 0
-	total_count_limit = 0  # поменяйте это значение, если вам нужны не все сообщения
-
+async def get_messages_at_date(channel, date, filename):
 	class DateTimeEncoder(json.JSONEncoder):
 		'''Класс для сериализации записи дат в JSON'''
 		def default(self, o):
@@ -77,34 +43,22 @@ async def dump_all_messages(channel):
 			if isinstance(o, bytes):
 				return list(o)
 			return json.JSONEncoder.default(self, o)
-
-	while True:
-		history = await client(GetHistoryRequest(
-			peer=channel,
-			offset_id=offset_msg,
-			offset_date=None, add_offset=0,
-			limit=limit_msg, max_id=0, min_id=0,
-			hash=0))
-		if not history.messages:
-			break
-		messages = history.messages
-		for message in messages:
-			all_messages.append(message.to_dict())
-		offset_msg = messages[len(messages) - 1].id
-		total_messages = len(all_messages)
-		if total_count_limit != 0 and total_messages >= total_count_limit:
-			break
-
-	with open('channel_messages.json', 'w', encoding='utf8') as outfile:
-		 json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
-
+	all_messages = []
+	async for msg in client.iter_messages(channel, reverse = True, offset_date=date):
+		all_messages.append(msg.text)
+	
+	with open(filename, 'w', encoding='utf8') as outfile:
+		json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
 
 async def main():
-	url = input("Введите ссылку на канал или чат: ")
+	#date_of_post = tz.localize(datetime.strptime(sys.argv[2], '%Y-%m-%d')) #datetime(2023, 5, 25, 0, 0, 0, tzinfo=utc) #parse('2023-05-28 00:00:00') #datetime.datetime(2023, 5, 28)
+	tz = pytz.timezone('Europe/Moscow')
+	date_of_post = parse(sys.argv[2]).replace(tzinfo=pytz.timezone('Europe/Moscow'))
+	url = sys.argv[1]
+	filename = sys.argv[3]
 	channel = await client.get_entity(url)
 	#await dump_all_participants(channel)
-	await dump_all_messages(channel)
+	await get_messages_at_date(channel, date_of_post, filename)
 
 with client:
 	client.loop.run_until_complete(main())
-    
