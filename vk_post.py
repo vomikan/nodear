78ef05c04c37eddf35df60a5faa55236b999a38b
vomikan <nikolay.akimov@gmail.com>
2023-07-01 14:19:08 +0300

    fix(vk): sender

---
+++ vk_post.py
@@ -0,0 +1,31 @@
+import vk_api
+import configparser
+import io
+import sys
+
+# Считываем учетные данные
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Присваиваем значения внутренним переменным
+id = config['VK']['owner_id'] # id пользователя / сообщества
+access_token = config['VK']['access_token'] 
+
+session=vk_api.VkApi(token=access_token)
+vk=session.get_api()
+
+sys.stdin.reconfigure(encoding='utf-8')
+text = ""
+for line in sys.stdin:
+    text += line
+
+
+def post():
+    vk.wall.post(
+        owner_id = id,  # id пользователя / сообщества
+        message = text,
+        copyright = 'https://t.me/MinisterstvoDizaina'
+    )
+
+if __name__=="__main__":
+    post()
